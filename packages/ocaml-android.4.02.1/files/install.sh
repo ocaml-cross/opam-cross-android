3e40997b6d4196accfb47319a64b9cbbc2078226
Peter Zotov <whitequark@whitequark.org>
2014-10-17 09:43:31 +0400

    Install default findlib packages alongside ocaml-android.

---
+++ packages/ocaml-android.4.02.1/files/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+PREFIX="$1"
+
+for pkg in bigarray bytes compiler-libs dynlink findlib graphics labltk num num-top ocamlbuild stdlib str threads unix; do
+  cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/arm-linux-androideabi/lib/"
+done
+
+mkdir -p "${PREFIX}/lib/findlib.conf.d"
+cp android.conf "${PREFIX}/lib/findlib.conf.d"
