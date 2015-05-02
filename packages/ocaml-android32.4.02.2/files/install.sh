2e05426a1b07b725cd972c67b1c276bcd2c59e4e
whitequark <whitequark@whitequark.org>
2015-05-02 17:30:34 +0300

    +ocaml-android.4.02.2 (EXPERIMENTAL).

---
+++ packages/ocaml-android32.4.02.2/files/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+PREFIX="$1"
+
+# remove after 4.02.2 bootstrap
+for bin in ocaml ocamlbuild ocamlbuild.byte ocamlc ocamlcp ocamldebug ocamldep ocamldoc ocamllex ocamlmklib ocamlmktop ocamlobjinfo ocamlopt ocamloptp ocamlprof ocamlrun ocamlyacc; do
+  path="${PREFIX}/arm-linux-androideabi/bin/${bin}"
+  if [ -e "${path}" ] && [ "$(head -c 1 ${path})" = "/" ]; then
+    /bin/echo -n '#!' | cat - "${path}" >"${path}.n"
+    mv "${path}.n" "${path}"
+    chmod +x "${path}"
+  fi
+done
+
+for pkg in bigarray bytes compiler-libs dynlink findlib graphics num num-top ocamlbuild stdlib str threads unix; do
+  cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/arm-linux-androideabi/lib/"
+done
+
+mkdir -p "${PREFIX}/lib/findlib.conf.d"
+cp android.conf "${PREFIX}/lib/findlib.conf.d"
