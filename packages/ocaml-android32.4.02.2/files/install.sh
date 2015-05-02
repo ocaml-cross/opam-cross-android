1e3033502b1a054540f78a6d5eb15124efe6711b
whitequark <whitequark@whitequark.org>
2015-05-02 18:49:21 +0300

    ocaml-android32.4.02.2: track upstream.

--- packages/ocaml-android32.4.02.2/files/install.sh
+++ packages/ocaml-android32.4.02.2/files/install.sh
@@ -2,16 +2,6 @@
 
 PREFIX="$1"
 
-# remove after 4.02.2 bootstrap
-for bin in ocaml ocamlbuild ocamlbuild.byte ocamlc ocamlcp ocamldebug ocamldep ocamldoc ocamllex ocamlmklib ocamlmktop ocamlobjinfo ocamlopt ocamloptp ocamlprof ocamlrun ocamlyacc; do
-  path="${PREFIX}/arm-linux-androideabi/bin/${bin}"
-  if [ -e "${path}" ] && [ "$(head -c 1 ${path})" = "/" ]; then
-    /bin/echo -n '#!' | cat - "${path}" >"${path}.n"
-    mv "${path}.n" "${path}"
-    chmod +x "${path}"
-  fi
-done
-
 for pkg in bigarray bytes compiler-libs dynlink findlib graphics num num-top ocamlbuild stdlib str threads unix; do
   cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/arm-linux-androideabi/lib/"
 done
