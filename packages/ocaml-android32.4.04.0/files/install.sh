405bbef25193a68c22e53e4523fb88fc33e5e5dc
juloo <juloo.dsi@gmail.com>
2018-07-10 18:02:25 +0200

    ocaml-android32.4.04.0: don't install graphics package

--- packages/ocaml-android32.4.04.0/files/install.sh
+++ packages/ocaml-android32.4.04.0/files/install.sh
@@ -2,7 +2,7 @@
 
 PREFIX="$1"
 
-for pkg in bigarray bytes compiler-libs dynlink findlib graphics num num-top stdlib str threads unix; do
+for pkg in bigarray bytes compiler-libs dynlink findlib num num-top stdlib str threads unix; do
   cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/android-sysroot/lib/"
 done
 
