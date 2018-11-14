a56eb723f76ae942b8d2f7137496cad2397949e6
Mark Hayden <mark@seaiq.com>
2018-11-14 15:50:46 -0500

    Added to repos

---
+++ packages/ocaml-android64.4.04.0/files/config/m.h
@@ -0,0 +1,15 @@
+#define ARCH_SIXTYFOUR
+#define SIZEOF_INT 4
+#define SIZEOF_LONG 8
+#define SIZEOF_PTR 8
+#define SIZEOF_SHORT 2
+#define SIZEOF_LONGLONG 8
+#define ARCH_INT64_TYPE long long
+#define ARCH_UINT64_TYPE unsigned long long
+#define ARCH_INT64_PRINTF_FORMAT "I64"
+#undef ARCH_BIG_ENDIAN
+#undef ARCH_ALIGN_DOUBLE
+#undef ARCH_ALIGN_INT64
+#undef NONSTANDARD_DIV_MOD
+#undef ASM_CFI_SUPPORTED
+#define PROFINFO_WIDTH 26
